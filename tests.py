89543f58cd86dfb45df6db6f0ad185751d7aba29
briziorusso <brizio.russo@gmail.com>
2024-02-15 17:13:41 +0000

    add tests up to six nodes

--- tests.py
+++ tests.py
@@ -18,12 +18,16 @@ __copyright__ = "Copyright (c) 2024 Fabrizio Russo"
 
 import unittest
 from causalaba import CausalABA
-from utils import model_to_set_of_arrows
+import networkx as nx
+import numpy as np
+import pandas as pd
+from utils import model_to_set_of_arrows, find_all_d_separations_sets
+
 
 class TestCausalABA(unittest.TestCase):
 
-    def three_node_graph_full(self):
-        print("===============Running three_node_graph_full===============")
+    def three_node_all_graphs(self):
+        print("===============Running three_node_all_graphs===============")
         num_of_nodes = 3
         expected = set([
             frozenset(),
@@ -91,10 +95,205 @@ class TestCausalABA(unittest.TestCase):
         model_sets = set()
         for model in models:
             arrows = model_to_set_of_arrows(model, num_of_nodes)
-            model_sets.add(frozenset(arrows))            
-        
+            model_sets.add(frozenset(arrows))
+
         self.assertEqual(model_sets, expected)
 
-TestCausalABA().three_node_graph_full()
+    def chains_confounder(self):
+        print("===============Running chains_confounder===============")
+        num_of_nodes = 3
+        expected = set([
+            ##chains
+            frozenset({(1, 2), (2, 0)}),
+            frozenset({(0, 2), (2, 1)}),
+            ##confounder
+            frozenset({(2, 0), (2, 1)})
+        ])
+        models = CausalABA(num_of_nodes, "encodings/test_lps/chains_confounder.lp")
+        
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))
+
+        self.assertEqual(model_sets, expected)
+
+    def one_edge(self):
+        print("===============Running one_edge===============")
+        num_of_nodes = 3
+        expected = set([
+            frozenset({(0, 1)}),
+            frozenset({(1, 0)}),
+        ])
+        models = CausalABA(num_of_nodes, "encodings/test_lps/one_edge.lp")
+        
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))
+
+        self.assertEqual(model_sets, expected)
+
+    def incompatible_Is(self):
+        print("===============Running incompatible_Is===============")
+        num_of_nodes = 3
+        expected = set()
+        models = CausalABA(num_of_nodes, "encodings/test_lps/incompatible_Is.lp")
+        
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))       
+
+        self.assertEqual(model_sets, expected)
+
+    def four_node_all_graphs(self):
+        print("===============Running four_node_graph_full===============")
+        num_of_nodes = 4
+        models = CausalABA(num_of_nodes, print_models=False)
+        
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))            
+        
+        self.assertEqual(len(model_sets), 543)
+
+    def four_node_example(self):
+        scenario = "four_node_example"
+        facts_location = f"encodings/test_lps/{scenario}.lp"
+        print(f"===============Running {scenario}===============")
+        B_true = np.array( [[ 0,  0,  0,  0],
+                            [ 0,  0,  0,  0],
+                            [ 1,  1,  0,  0],
+                            [ 0,  1,  1,  0],
+                            ])
+        num_of_nodes = B_true.shape[0]
+        print(B_true)
+        G_true = nx.DiGraph(pd.DataFrame(B_true.T, columns=[f"X{i+1}" for i in range(B_true.shape[1])], index=[f"X{i+1}" for i in range(B_true.shape[1])]))
+        print(G_true.edges)
+
+        expected = set([
+            frozenset({(0, 2), (1, 2), (1, 3), (2, 3)})
+        ])
+
+        true_seplist = find_all_d_separations_sets(G_true)
+
+        with open(facts_location, "w") as f:
+            for s in true_seplist:
+                f.write(s + "\n")
+
+        models = CausalABA(num_of_nodes, facts_location)
+
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))            
+
+        self.assertEqual(set(model_sets), expected)
+
+    def five_node_all_graphs(self):
+        print("===============Running five_node_all_graphs===============")
+        num_of_nodes = 5
+        models = CausalABA(num_of_nodes, print_models=False)
+        
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))            
+        
+        self.assertEqual(len(model_sets), 29281)
+
+    def five_node_colombo_example(self):
+        scenario = "five_node_colombo_example"
+        facts_location = f"encodings/test_lps/{scenario}.lp"
+        print(f"===============Running {scenario}===============")
+        B_true = np.array( [[ 0,  0,  0,  0],
+                            [ 0,  0,  0,  0],
+                            [ 1,  1,  0,  0],
+                            [ 0,  1,  1,  0],
+                            ])
+        num_of_nodes = B_true.shape[0]
+        print(B_true)
+        G_true = nx.DiGraph(pd.DataFrame(B_true.T, columns=[f"X{i+1}" for i in range(B_true.shape[1])], index=[f"X{i+1}" for i in range(B_true.shape[1])]))
+        print(G_true.edges)
+
+        expected = set([
+            frozenset({(0, 2), (1, 2), (1, 3), (2, 3)})
+        ])
+
+        true_seplist = find_all_d_separations_sets(G_true)
+
+        with open(facts_location, "w") as f:
+            for s in true_seplist:
+                f.write(s + "\n")
+
+        models = CausalABA(num_of_nodes, facts_location)
+
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))            
+
+        self.assertEqual(set(model_sets), expected)
+
+    def six_node_all_graphs(self):
+        print("===============Running six_node_all_graphs===============")
+        num_of_nodes = 6
+        models = CausalABA(num_of_nodes, print_models=False)
+        
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))            
+        
+        self.assertEqual(len(model_sets), 3781503)
+
+
+    def six_node_example(self):
+        scenario = "six_node_example"
+        facts_location = f"encodings/test_lps/{scenario}.lp"
+        print(f"===============Running {scenario}===============")
+        B_true = np.array( [[ 0,  0,  0,  0,  0,  0],
+                            [ 0,  0,  0,  0,  0,  0],
+                            [ 1,  1,  0,  0,  0,  0],
+                            [ 1,  0,  1,  0,  0,  0],
+                            [ 1,  0,  1,  0,  0,  0],
+                            [ 1,  1,  1,  1,  1,  0]])
+        num_of_nodes = B_true.shape[0]
+        print(B_true)
+        G_true = nx.DiGraph(pd.DataFrame(B_true.T, columns=[f"X{i+1}" for i in range(B_true.shape[1])], index=[f"X{i+1}" for i in range(B_true.shape[1])]))
+        print(G_true.edges)
+
+        inv_nodes_dict = {n:int(n.replace("X",""))-1 for n in G_true.nodes()}
+        G_true1 = nx.relabel_nodes(G_true, inv_nodes_dict)
+
+        expected = {frozenset(list(G_true1.edges()))}
+
+        true_seplist = find_all_d_separations_sets(G_true)
+
+        with open(facts_location, "w") as f:
+            for s in true_seplist:
+                f.write(s + "\n")
+
+        models = CausalABA(num_of_nodes, facts_location)
+
+        model_sets = set()
+        for model in models:
+            arrows = model_to_set_of_arrows(model, num_of_nodes)
+            model_sets.add(frozenset(arrows))            
+
+        self.assertEqual(model_sets, expected)
+
+TestCausalABA().three_node_all_graphs()
 TestCausalABA().three_node_graph_empty()
 TestCausalABA().collider()
+TestCausalABA().chains_confounder()
+TestCausalABA().one_edge()
+TestCausalABA().incompatible_Is()
+TestCausalABA().four_node_all_graphs()
+TestCausalABA().four_node_example()
+TestCausalABA().five_node_all_graphs()
+TestCausalABA().five_node_colombo_example()
+# TestCausalABA().six_node_all_graphs() ## This test takes 8 minutes to run, 3.7M models
+TestCausalABA().six_node_example()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from itertools import chain, combinations
 import numpy as np
+import networkx as nx
 
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
@@ -19,3 +20,34 @@ def model_to_set_of_arrows(model:list, num_of_nodes:int)->set:
         if atom.name == 'arrow':
             arrows.add((atom.arguments[0].number,atom.arguments[1].number))
     return arrows
+
+def find_all_d_separations_sets(G, verbose=True, debug=False):
+    no_of_var = len(G.nodes)
+    septests = []
+    for comb in combinations(range(no_of_var), 2):
+        if debug:
+            print(comb)
+        if comb[0] != comb[1]:
+            x = comb[0]
+            y = comb[1]
+            if debug:
+                print(x,y)
+            depth = 0
+            while no_of_var-1 > depth:
+                Neigh_x_noy = [f"X{k+1}" for k in range(no_of_var) if k != x and k != y]
+                if debug:
+                    print(Neigh_x_noy)
+                for S in combinations(Neigh_x_noy, depth):
+                    if debug:
+                        print(S)
+                    s = set([int(s.replace('X',''))-1 for s in S])
+                    s_str = 'empty' if len(S)==0 else 's'+''.join([str(i) for i in s])
+                    if nx.algorithms.d_separated(G, {f"X{x+1}"}, {f"X{y+1}"}, set(S)):
+                        if verbose:
+                            print(f"X{x+1} and X{y+1} are d-separated by {S}")
+                        septests.append(f"indep({x},{y},{s_str}).")
+                    else:
+                        # print(f"X{x+1} and X{y+1} are not d-separated by {S}")
+                        septests.append(f"dep({x},{y},{s_str}).")
+                depth += 1
+    return septests
