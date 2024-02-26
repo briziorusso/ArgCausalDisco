e597a6b4ffc5a9b8c20cf4f5958ce9458160bd76
briziorusso <brizio.russo@gmail.com>
2024-02-26 19:00:52 +0000

    working external tiny example

--- encodings/test_lps/test_external.py
+++ encodings/test_lps/test_external.py
@@ -7,9 +7,14 @@ ctl.configuration.solve.parallel_mode = os.cpu_count()
 ctl.configuration.solve.models="0"
 ctl.configuration.solver.seed="2024"
 
-ctl.load("encodings/test_lps/test_external.lp")
+ctl.load("test_external.lp")
 
 ctl.ground([("base", [])])
+
+ctl.assign_external(Function("ext_ar"), True)
+ctl.assign_external(Function("b"), True)
+ctl.assign_external(Function("ext_indep", [Number(0), Number(1), Function("empty")]), True)
+
 models = []
 count_models = 0
 with ctl.solve(yield_=True) as handle:
