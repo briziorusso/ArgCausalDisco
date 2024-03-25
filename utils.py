b3fd9db65063f308fcaa3404f948515263e9ea48
briziorusso <brizio.russo@gmail.com>
2024-03-25 17:16:48 +0000

    add freer_gpu() function for other models

--- utils.py
+++ utils.py
@@ -56,6 +56,14 @@ def logger_setup(output_file:str=""):
     if len(logging.getLogger('').handlers) < 2:
         logging.getLogger('').addHandler(console)
 
+# @profile
+def get_freer_gpu():
+    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
+    if len(memory_available)>0:
+        return np.argmax(memory_available)
+    elif torch.cuda.is_available():
+        return 0
 
 def random_stability(seed_value=0, deterministic=True, verbose=False):
     '''
