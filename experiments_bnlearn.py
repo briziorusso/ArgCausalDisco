8a04450ed7b687dd282503c6a783ca8d9e6818a3
briziorusso <brizio.russo@gmail.com>
2024-05-14 13:14:43 +0100

    update bnlearn experiments

--- experiments_bnlearn.py
+++ experiments_bnlearn.py
@@ -23,77 +23,109 @@ import pandas as pd
 from datetime import datetime
 from tqdm.auto import tqdm
 from models import run_method
-from utils import load_bnlearn_data_dag, DAGMetrics, random_stability, simulate_dag, logger_setup
+from utils import load_bnlearn_data_dag, DAGMetrics, random_stability, simulate_dag, logger_setup, dag2cpdag
 
 import warnings
 warnings.filterwarnings("ignore")
 
-version = 'bnlearn_wstd_disco_kci'
+version = 'bnlearn_50rep'
 logger_setup(f'results/log_{version}.log')
+data_path = '../ShapleyPC-local/datasets'
+sample_size = 5000
+n_runs = 50
 device = 0
-print_graphs = False
+load_res = True
 save_res = True
-load_res = False
-dataset_list = ['cancer', #'earthquake', 
-                'survey', 'asia'
-                # , 'sachs' 
-                #, 'alarm', 'insurance', 'child', 'hailfinder',  'hepar2'
+dataset_list = [
+                'cancer', 
+                'earthquake', 
+                'survey', 
+                'asia'
                 ]
 model_list = [
             'random'
-            ,'spc' 
-            # ,'pc_max'
-            # ,'cam'
+            ,'mpc'
+            ,'abapc'
+            ,'fgs'
             ,'nt' 
-            # ,'mcsl'
-            ,'fgs',
-            'abapc'
     ]
-data_path = '../ShapleyPC-local/datasets'
-sample_size = 2000
 
-mt_res = pd.DataFrame()
+if load_res:         
+    mt_res = pd.DataFrame(np.load(f"results/stored_results_{version}.npy", allow_pickle=True), 
+                       columns = ['dataset', 'model', 'elapsed_mean', 'elapsed_std', 'nnz_mean', 'nnz_std', 
+                                'fdr_mean', 'fdr_std', 'tpr_mean', 'tpr_std', 'fpr_mean', 'fpr_std', 
+                                'precision_mean', 'precision_std', 'recall_mean', 'recall_std',
+                                'F1_mean', 'F1_std', 'shd_mean', 'shd_std','sid_mean', 'sid_std'])
+    mt_res_cpdag = pd.DataFrame(np.load(f"results/stored_results_{version}_cpdag.npy", allow_pickle=True), 
+                       columns = ['dataset', 'model', 'elapsed_mean', 'elapsed_std', 'nnz_mean', 'nnz_std', 
+                                'fdr_mean', 'fdr_std', 'tpr_mean', 'tpr_std', 'fpr_mean', 'fpr_std', 
+                                'precision_mean', 'precision_std', 'recall_mean', 'recall_std',
+                                'F1_mean', 'F1_std', 'shd_mean', 'shd_std', 'sid_low_mean', 'sid_low_std', 'sid_high_mean', 'sid_high_std'])
+    ## save backup to npy
+    np.save(f"results/stored_results_{version}_bkp.npy", mt_res )
+    np.save(f"results/stored_results_{version}_cpdag_bkp.npy", mt_res_cpdag )
+else:
+    mt_res = pd.DataFrame()
+    mt_res_cpdag = pd.DataFrame()
+
 for dataset_name in dataset_list:
-    if load_res:
-        mt_res = np.load(f"results/stored_results_{version}.npy", allow_pickle=True)
-    else:
-        ##Load data
-        X_s, B_true = load_bnlearn_data_dag(dataset_name, data_path, sample_size, seed=2023, print_info=True, standardise=True)
-        # np.save(f"{dataset_name}_data.npy", X_s)
+    names_dict = {'pc':'PC', 'pc_max':'Max-PC', 'fgs':'FGS', 'spc':'Shapley-PC', 'mpc':'MPC', 'cpc':'CPC', 'abapc':'ABAPC (Ours)', 'cam':'CAM', 'nt':'NOTEARS-MLP', 'mcsl':'MCSL-MLP', 'ges':'GES', 'random':'Random'}
+    # B_true = nx.adjacency_matrix(true_causal_matrix).todense()
 
-        names_dict = {'pc_max':'Max-PC', 'fgs':'FGS', 'spc':'Shapley-PC', 'abapc':'ABAPC (Ours)', 'cam':'CAM', 'nt':'NOTEARS-MLP', 'mcsl':'MCSL-MLP', 'ges':'GES', 'random':'Random'}
-        # B_true = nx.adjacency_matrix(true_causal_matrix).todense()
-
-        for method in model_list:
-            random_stability(2023)
-            seeds_list = np.random.randint(0, 10000, (10, )).tolist()
-            seeds_list = [seeds_list[0]] if method in ['abapc', 'spc', 'pc_max', 'cam', 'fgs'] else seeds_list
-            logging.debug(f'Seeds:{seeds_list}')
-            logging.info(f"Running {method}")
-            
-            method_res = []
-            for seed in seeds_list:
-                if method=='random':
-                    random_stability(seed)
-                    start = datetime.now()
-                    B_est = simulate_dag(d=B_true.shape[1], s0=B_true.sum().astype(int), graph_type='ER')
-                    elapsed = (datetime.now()-start).total_seconds()
-                    mt = DAGMetrics(B_est, B_true)
+    for method in model_list:
+        random_stability(2024)
+        seeds_list = np.random.randint(0, 10000, (n_runs, )).tolist()
+        # seeds_list = [seeds_list[0]] if method in ['abapc', 'spc', 'pc_max', 'cam', 'fgs', 'pc', 'mpc', 'cpc'] else seeds_list
+        logging.debug(f'Seeds:{seeds_list}')
+        logging.info(f"Running {method}")
+        
+        method_res = []
+        method_res_cpdag = []
+        for seed in seeds_list:
+            ##Load data
+            X_s, B_true = load_bnlearn_data_dag(dataset_name, data_path, sample_size, seed=seed, print_info=True if seed == seeds_list[0] else False, standardise=True)
+            if method=='random':
+                random_stability(seed)
+                start = datetime.now()
+                B_est = simulate_dag(d=B_true.shape[1], s0=B_true.sum().astype(int), graph_type='ER')
+                elapsed = (datetime.now()-start).total_seconds()
+                mt_cpdag = DAGMetrics(dag2cpdag(B_est), B_true).metrics
+                mt_dag = DAGMetrics(B_est, B_true).metrics
+            else:
+                W_est, elapsed = run_method(X_s, method, seed, test_alpha=0.01, test_name='fisherz', device=device, scenario=f"{method}_{version}_{dataset_name}")
+                logger_setup(f'results/log_{version}.log', continue_logging=True)
+                if W_est is None:
+                    mt_cpdag = {'nnz':np.nan, 'fdr':np.nan, 'tpr':np.nan, 'fpr':np.nan, 'precision':np.nan, 'recall':np.nan, 'F1':np.nan, 'shd':np.nan, 'sid':np.nan}
+                    mt_dag = {'nnz':np.nan, 'fdr':np.nan, 'tpr':np.nan, 'fpr':np.nan, 'precision':np.nan, 'recall':np.nan, 'F1':np.nan, 'shd':np.nan, 'sid':np.nan}
                 else:
-                    W_est, elapsed = run_method(X_s, method, seed, test_alpha=0.01, test_name='kci', device=device, scenario=f"{method}_{version}_{dataset_name}")
-                    logger_setup(f'results/log_{version}.log', continue_logging=True)
-                    if W_est is None:
-                        mt.metrics = {'nnz':np.nan, 'fdr':np.nan, 'tpr':np.nan, 'fpr':np.nan, 'precision':np.nan, 'recall':np.nan, 'F1':np.nan, 'shd':np.nan, 'sid':np.nan}
-                    else:
-                        B_est = (W_est > 0).astype(int)
-                        mt = DAGMetrics(B_est, B_true)
+                    B_est = (W_est != 0).astype(int)
+                    mt_cpdag = DAGMetrics(dag2cpdag(B_est), B_true).metrics
+                    B_est = (W_est > 0).astype(int)
+                    mt_dag = DAGMetrics(B_est, B_true).metrics
+            # calculate metrics
+            logging.info({'dataset':dataset_name, 'model':names_dict[method], 'elapsed':elapsed , **mt_dag})
+            logging.info({'dataset':dataset_name, 'model':names_dict[method], 'elapsed':elapsed , **mt_cpdag})
+            
+            method_res.append({'dataset':dataset_name, 'model':names_dict[method], 'elapsed':elapsed , **mt_dag})
+            if type(mt_cpdag['sid'])==tuple:
+                mt_sid_low = mt_cpdag['sid'][0]
+                mt_sid_high = mt_cpdag['sid'][1]
+            else:
+                mt_sid_low = mt_cpdag['sid']
+                mt_sid_high = mt_cpdag['sid']
+            mt_cpdag.pop('sid')
+            mt_cpdag['sid_low'] = mt_sid_low
+            mt_cpdag['sid_high'] = mt_sid_high
+            method_res_cpdag.append({'dataset':dataset_name, 'model':names_dict[method], 'elapsed':elapsed , **mt_cpdag})
+            
+        method_sum = pd.DataFrame(method_res).groupby(['dataset','model'], as_index=False).agg(['mean','std']).round(2).reset_index(drop=True)
+        method_sum.columns = method_sum.columns.map('_'.join).str.strip('_')
+        mt_res = pd.concat([mt_res, method_sum], sort=False)
 
-                # calculate metrics
-                logging.info({'dataset':dataset_name, 'model':names_dict[method], 'elapsed':elapsed , **mt.metrics})
-                method_res.append({'dataset':dataset_name, 'model':names_dict[method], 'elapsed':elapsed , **mt.metrics})
-            method_sum = pd.DataFrame(method_res).groupby(['dataset','model'], as_index=False).agg(['mean','std']).round(2).reset_index(drop=True)
-            method_sum.columns = method_sum.columns.map('_'.join).str.strip('_')
-            mt_res = pd.concat([mt_res, method_sum], sort=False)
+        method_sum = pd.DataFrame(method_res_cpdag).groupby(['dataset','model'], as_index=False).agg(['mean','std']).round(2).reset_index(drop=True)
+        method_sum.columns = method_sum.columns.map('_'.join).str.strip('_')
+        mt_res_cpdag = pd.concat([mt_res_cpdag, method_sum], sort=False)
 
-            if save_res:
-                np.save(f"results/stored_results_{version}.npy", mt_res )
+        if save_res:
+            np.save(f"results/stored_results_{version}.npy", mt_res )
+            np.save(f"results/stored_results_{version}_cpdag.npy", mt_res_cpdag )
